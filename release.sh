b9f34c4b2dce6ddd76d09798567ce8825ea5cf4c
Loudbook <loudbooked@gmail.com>
2024-04-11 23:01:02 -0700

    Cleanup

--- release.sh
+++ release.sh
@@ -1,5 +1,3 @@
-cargo build --release --target x86_64-unknown-linux-gnu
-cargo build --release --target aarch64-unknown-linux-gnu
 cargo build --release --target x86_64-apple-darwin
 cargo build --release --target aarch64-apple-darwin
 cargo build --release --target x86_64-pc-windows-gnu
@@ -8,6 +6,4 @@ mkdir target/out
 
 cp target/x86_64-apple-darwin/release/fullreport target/out/fullreport-macos-x86_64
 cp target/aarch64-apple-darwin/release/fullreport target/out/fullreport-macos-aarch64
-cp target/x86_64-unknown-linux-gnu/release/fullreport target/out/fullreport-linux-x86_64
-cp target/aarch64-unknown-linux-gnu/release/fullreport target/out/fullreport-linux-aarch64
 cp target/x86_64-pc-windows-gnu/release/fullreport.exe target/out/fullreport-windows-x86_64.exe
