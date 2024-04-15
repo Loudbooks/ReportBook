c09acccff68a8a327ca6e5057778b69e2cb4f602
Loudbook <loudbooked@gmail.com>
2024-04-15 19:01:06 -0400

    Add MacOS automatic run

---
+++ macos-run.sh
@@ -0,0 +1,12 @@
+mkdir ./temp
+
+if [[ $(uname -m) == 'arm64' ]]; then
+    cd ./temp && curl -LO "https://github.com/Loudbooks/ReportBook/releases/download/latest/reportbook-macos-aarch64"
+else 
+    cd ./temp && curl -LO "https://github.com/Loudbooks/ReportBook/releases/download/latest/reportbook-macos-x86_64"
+fi
+
+chmod +x ./reportbook-macos-aarch64
+./reportbook-macos-aarch64
+
+rm -r ./temp
