75e57207807b00b924da8938f0679d3ec5b0696c
Loudbook <loudbooked@gmail.com>
2024-10-06 23:03:48 -0400

    Initial UI Commit

---
+++ src-tauri/src/reportbook/mod.rs
@@ -0,0 +1,5 @@
+pub mod reportbook;
+mod app;
+mod file_handler;
+mod process;
+mod datagatherers;
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<!--
+<script lang="ts">
+  import { invoke } from "@tauri-apps/api/core";
+
+  async function requestReport() {
+    await invoke("requestReport");
+  }
+</script>-->
