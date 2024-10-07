6306b8350ee4bad067df027670228b5dd7835507
Loudbook <loudbooked@gmail.com>
2024-10-07 19:33:26 -0400

    Add remaining glyphs

---
+++ src/routes/glyph/Loading.svelte
@@ -0,0 +1,25 @@
+<svg width="800px" height="800px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="none" class="hds-flight-icon--animation-loading">
+    <g fill="gray" fill-rule="evenodd" clip-rule="evenodd">
+        <path d="M8 1.5a6.5 6.5 0 100 13 6.5 6.5 0 000-13zM0 8a8 8 0 1116 0A8 8 0 010 8z" opacity=".2"/>
+        <path d="M7.25.75A.75.75 0 018 0a8 8 0 018 8 .75.75 0 01-1.5 0A6.5 6.5 0 008 1.5a.75.75 0 01-.75-.75z"/>
+    </g>
+</svg>
+
+<style>
+    svg {
+        height: 15px;
+        width: 15px;
+        overflow: hidden;
+
+        animation: loading 1s linear infinite;
+    }
+
+    @keyframes loading {
+        0% {
+            transform: rotate(0deg);
+        }
+        100% {
+            transform: rotate(360deg);
+        }
+    }
+</style>
