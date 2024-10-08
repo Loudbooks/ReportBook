d904690d9ac5f53a07ae5a20876b2c73ff3adaa9
Loudbook <loudbooked@gmail.com>
2024-10-07 22:22:24 -0400

    Update Version

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,6 +1,6 @@
 import { writable } from 'svelte/store';
 
-export const VERSION = "v0.1.0"
+export const VERSION = "v2.0.0"
 
 export const report = writable("")
 export const preferredUsername = writable("")
