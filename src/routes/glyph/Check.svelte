2b0d6d7f903edf09a4b1219b29e5f1c79b659bb2
Loudbook <loudbooked@gmail.com>
2024-10-07 21:54:52 -0400

    Fix gray

--- src/routes/glyph/Check.svelte
+++ src/routes/glyph/Check.svelte
@@ -1,5 +1,5 @@
 <svg width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M4 12.6111L8.92308 17.5L20 6.5" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M4 12.6111L8.92308 17.5L20 6.5" stroke="gray" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>
 
 <style>
