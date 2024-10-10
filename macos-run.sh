cf11c2bb87658f75a29108cab62c00f1edf1c179
Loudbook <loudbooked@gmail.com>
2024-10-09 22:34:42 -0400

    Silence

--- macos-run.sh
+++ macos-run.sh
@@ -1,10 +1,12 @@
 if [[ $(uname -m) == 'arm64' ]]; then
-    cd /tmp && curl -LO "https://github.com/Loudbooks/ReportBook/releases/latest/download/reportbook-macos-aarch64"
+    echo Downloading the aarch64 version of ReportBook...
+    cd /tmp && curl -LO -s "https://github.com/Loudbooks/ReportBook/releases/latest/download/reportbook-macos-aarch64"
     chmod +x ./reportbook-macos-aarch64
     ./reportbook-macos-aarch64
     rm ./reportbook-macos-aarch64
 else
-    cd /tmp && curl -LO "https://github.com/Loudbooks/ReportBook/releases/latest/download/reportbook-macos-x86_64"
+    echo Downloading the x86_64 version of ReportBook...
+    cd /tmp && curl -LO -s "https://github.com/Loudbooks/ReportBook/releases/latest/download/reportbook-macos-x86_64"
     chmod +x ./reportbook-macos-x86_64
     ./reportbook-macos-x86_64
     rm ./reportbook-macos-x86_64
