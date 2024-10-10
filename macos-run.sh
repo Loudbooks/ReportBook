d3431330c80440e06c84de63961a1804e786fc72
Loudbook <loudbooked@gmail.com>
2024-10-09 22:23:18 -0400

    Update macos-run.sh

--- macos-run.sh
+++ macos-run.sh
@@ -1,4 +1,11 @@
-  cd /tmp && curl -LO "https://github.com/Loudbooks/ReportBook/releases/latest/download/reportbook"
-  chmod +x ./reportbook
-  ./reportbook
-  rm ./reportbook
+if [[ $(uname -m) == 'arm64' ]]; then
+    cd /tmp && curl -LO "https://github.com/Loudbooks/ReportBook/releases/latest/download/reportbook-macos-aarch64"
+    chmod +x ./reportbook-macos-aarch64
+    ./reportbook-macos-aarch64
+    rm ./reportbook-macos-aarch64
+else
+    cd /tmp && curl -LO "https://github.com/Loudbooks/ReportBook/releases/latest/download/reportbook-macos-x86_64"
+    chmod +x ./reportbook-macos-x86_64
+    ./reportbook-macos-x86_64
+    rm ./reportbook-macos-x86_64
+fi
